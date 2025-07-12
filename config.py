6b41021f4e59c355d8de3db4f376611de5381cd2
Giuseppe Albanese <giuseppe.albanese@nrb.be>
2025-07-12 12:40:10 +0200

    CPUCT 4.0

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ config_params = {
 
     # -- MCTS Settings --
     "MCTS_SIMULATIONS": 800,
-    "CPUCT": 2.5,
+    "CPUCT": 4.0,
     
     # -- Tactical Puzzle Settings --
     "TACTICAL_PUZZLE_FILENAME": "tactical_puzzles.jsonl",
